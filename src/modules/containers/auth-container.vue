2274091381715d6d39efbe4bed54038056920af9
Huy Bui Dac <>
2020-11-22 12:30:09 +0700

    fix production

--- src/modules/containers/auth-container.vue
+++ src/modules/containers/auth-container.vue
@@ -3,9 +3,9 @@
     <v-card class="pa-0 ma-auto" max-width="1024" min-height="504">
       <v-row no-gutters style="min-height: 504px">
         <v-col cols="12" md="5" class="primary py-5 py-md-16">
-          <div class="d-flex fill-height justify-center align-end">
+          <v-row class="fill-height justify-center align-end">
             <v-img max-width="266" src="@/assets/logo_sologan.svg"></v-img>
-          </div>
+          </v-row>
         </v-col>
         <v-col cols="12" md="7">
           <div class="px-4 px-md-16 py-12">
