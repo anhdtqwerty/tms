3dae8f3f71d465fae7ef3f8b19b32f6f0aad4bd4
ngoc.tranquy <danhngoc197@gmail.com>
2021-04-13 22:52:49 +0700

    add rules file size

--- src/components/vuetify/app-file-input.vue
+++ src/components/vuetify/app-file-input.vue
@@ -7,6 +7,9 @@
     append-icon="publish"
     :accept="accept"
     v-bind="$attrs"
+    :rules="rules"
+    show-size
+    small-chips
     v-model="syncedValue"
   ></v-file-input>
 </template>
@@ -17,6 +20,14 @@ import { Component, PropSync, Vue } from 'vue-property-decorator'
 @Component
 export default class AppFileInput extends Vue {
   @PropSync('value', { default: null }) syncedValue: string
+
+  rules = [
+    (files: File[]) => {
+      const invalidFile = files.find(f => f.size > 104857600)
+      if (invalidFile) return 'File đính kèm không quá 100MB'
+      else return true
+    }
+  ]
 
   accept =
     'application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, application/pdf, application/x-rar-compressed, text/plain, application/zip'
