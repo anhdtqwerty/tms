d11ff8e9467d8f49fd7aab20968ce3cdbca8e2fd
ngoc.tranquy <danhngoc197@gmail.com>
2021-07-14 09:59:09 +0700

    update login

--- src/modules/containers/auth-container.vue
+++ src/modules/containers/auth-container.vue
@@ -5,15 +5,17 @@
     <v-card class="pa-0 ma-auto" max-width="1024" min-height="504">
       <v-row no-gutters style="min-height: 504px">
         <v-col cols="12" md="5" class="primary py-5 py-md-16 d-flex flex-column align-center px-16">
-          <div class="title text-center white--text mt-8">Phần mềm quản lý và theo dõi nhiệm vụ của Bộ GTVT</div>
-          <v-spacer></v-spacer>
-          <v-img max-width="100" max-height="70" src="@/assets/ministry-logo.svg"></v-img>
+          <v-img width="160" max-height="160" src="@/assets/logo.png"></v-img>
           <div class="white--text logo-title mt-2">
             BỘ GIAO THÔNG VẬN TẢI
           </div>
           <div class="white--text logo-subtitle">
             MINISTRY OF TRANSPORT
           </div>
+          <div class="name text-center white--text mt-16">PHẦN MỀM</div>
+          <div class="name text-center white--text ">QUẢN LÝ VÀ THEO DÕI</div>
+          <div class="name text-center white--text">NHIỆM VỤ CỦA BỘ GTVT</div>
+          <v-spacer></v-spacer>
         </v-col>
         <v-col cols="12" md="7">
           <div class="px-4 px-md-16 py-5 d-flex flex-column justify-center mt-4" style="height: 100%">
@@ -64,24 +66,25 @@ export default class AuthContainer extends Vue {
   background-color: #1e88e5;
   opacity: 0.3;
 }
-.title {
+.name {
+  font-family: 'Times New Roman';
   font-style: normal;
   font-weight: bold;
-  font-size: 20px;
-  line-height: 23px;
+  font-size: 24px;
+  line-height: 35px;
 }
 .logo-title {
   font-family: 'Times New Roman';
   font-style: normal;
   font-weight: bold;
-  font-size: 14px;
-  line-height: 20px;
+  font-size: 16px;
+  line-height: 30px;
 }
 .logo-subtitle {
   font-family: 'Times New Roman';
   font-style: normal;
   font-weight: bold;
-  font-size: 9px;
+  font-size: 12px;
   line-height: 20px;
 }
 </style>
